76d437cb3ebd2efe342d13d720f2e17a4616ed58
brutalnumberboy <brutalnumberboy@omg.lol>
2025-02-16 00:38:11 +0100

    add data to context

--- API/Context/StoreContext.cs
+++ API/Context/StoreContext.cs
@@ -8,6 +8,31 @@ namespace API.Context
         public StoreContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<Album> album {get; set;} 
+        public DbSet<Album> Albums { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Album>().HasData(new Album
+            {
+                AlbumId = 1,
+                Genre = "Metal",
+                Description = "None",
+                YearReleased = 1992,
+                Artist = "Darkthrone",
+                Title = "A Blaze in The Northern Sky",
+                Length = 42,
+            });
+            modelBuilder.Entity<Album>().HasData(new Album
+            {
+                AlbumId = 2,
+                Genre = "Metal",
+                Description = "None",
+                YearReleased = 1984,
+                Artist = "Metllica",
+                Title = "Master of Puppets",
+                Length = 55,
+            });
+        }
+
     }
 }
