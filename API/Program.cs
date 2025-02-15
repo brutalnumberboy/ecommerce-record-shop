9e91d81226a976f9453d3b8d3d249e0c980015ec
brutalnumberboy <brutalnumberboy@omg.lol>
2025-02-16 00:38:03 +0100

    add AutoMapper

--- API/Program.cs
+++ API/Program.cs
@@ -6,9 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer("Default Connection"));
+builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer("Server=localhost;Database=store;User id=sa;Pwd=Pas_w0rd;Encrypt=True;TrustServerCertificate=True;"));
 
 var app = builder.Build();
 
