d8a437037b0bba1c93f4036e03dc74b4ea707494
Timur Churov <brutalnumberboy@omg.lol>
2025-02-16 16:24:05 +0100

    provide default identity

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<StoreContext>();
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<StoreContext>().AddDefaultTokenProviders();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
 {
@@ -21,9 +21,7 @@ builder.Services.AddOpenApiDocument(config =>
     config.Title = "StoreAPI v1";
     config.Version = "v1";
 });
-builder.Services.AddIdentity<User, IdentityRole>()
-    .AddEntityFrameworkStores<StoreContext>()
-    .AddDefaultTokenProviders();
+
 
 var app = builder.Build();
 
