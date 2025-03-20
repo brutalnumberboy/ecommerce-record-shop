c39acdbf09b6b7d4404b3b83d4509b039eaa6012
Timur Churov <brutalnumberboy@omg.lol>
2025-03-20 17:05:26 +0100

    make user dto fields required

--- API/Mapping/DTO/UserDTO.cs
+++ API/Mapping/DTO/UserDTO.cs
@@ -8,8 +8,8 @@ namespace API.Mapping.DTO
 {
     public class UserDTO
     {
-        public string UserName {get; set;}
-        public string Email {get; set;}
-        public string Token {get; set;}
+        public required string UserName {get; set;}
+        public required string Email {get; set;}
+        public required string Token {get; set;}
     }
 }
