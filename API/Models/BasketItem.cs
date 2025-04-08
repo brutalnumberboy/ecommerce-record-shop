b2dc2344b4b2befd4516542f514b3bb1fbc16405
Timur Churov <brutalnumberboy@omg.lol>
2025-04-08 22:13:14 +0200

    make ID fileds primary keys

--- API/Models/BasketItem.cs
+++ API/Models/BasketItem.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Models
 {
     public class BasketItem
     {
-        public required int Id {get; set;}
+        [Key]
+        public int Id {get; set;}
         public required int Amount {get; set;}
         public required string Title {get; set;}
         public required int Price {get; set;}
--- API/Models/UserBasket.cs
+++ API/Models/UserBasket.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using API.Mapping.DTO;
 
 namespace API.Models
 {
     public class UserBasket
     {
+        [Key]
+        public int Id { get; set; }
         public required List<BasketItem> BasketItems { get; set; } = new List<BasketItem>();
         public int TotalPrice { get; set; }
         public required string ShippingAddress {get; set;}
