55f9fe891fbc6da19ddb3febcb00128259a0ca4f
Timur Churov <brutalnumberboy@omg.lol>
2025-04-10 20:16:13 +0200

    remove debug info and slight functional changes

--- RecordStoreUI/src/app/homepage/homepage.component.ts
+++ RecordStoreUI/src/app/homepage/homepage.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { AlbumService } from '../album.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   standalone: true,
   providers: [AlbumService],
-  // Note: 'providers' is used here to provide the AlbumService for this component only.
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
@@ -15,18 +15,15 @@ export class HomepageComponent implements OnInit {
   randomAlbums: any[] = [];
 
   constructor(private albumService: AlbumService) {
-    console.log('HomepageComponent constructor called'); // Debugging
   }
 
   ngOnInit(): void {
-    this.fetchRandomAlbums();
-    console.log('HomepageComponent initialized'); // Debugging
+    this.getRandomAlbums();
   }
 
-  fetchRandomAlbums(): void {
+  getRandomAlbums(): void {
     const observer = {
       next: (data: any) => {
-        console.log('Random albums fetched:', data); // Debugging
         this.randomAlbums = data;
       },
       error: (error: any) => {
