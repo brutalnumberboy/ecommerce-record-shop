f105b2a2eb04202f5cd9706002a9eb5de4dde689
Timur Churov <brutalnumberboy@omg.lol>
2025-04-12 22:10:29 +0200

    remove id column override

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -7,8 +7,6 @@ namespace API.Models
 {
     public class User : IdentityUser
     {
-        [Key]
-        public int Id {get; set;}
         [Required(AllowEmptyStrings = false)]
         [RegularExpression(@"^[a-zA-Z0-9]+$")]
         public override string UserName {get; set;}
