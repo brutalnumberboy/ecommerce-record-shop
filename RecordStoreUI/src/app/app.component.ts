d297358598d57dbd66e9cbacea0fca2d753c2049
Timur Churov <brutalnumberboy@omg.lol>
2025-04-17 07:18:54 +0200

    remove HomePage component import

--- RecordStoreUI/src/app/app.component.ts
+++ RecordStoreUI/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomepageComponent } from "./homepage/homepage.component";
 
 @Component({
   selector: 'app-root',
