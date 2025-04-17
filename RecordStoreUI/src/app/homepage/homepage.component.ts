da41e6eca4b98c76ed66873215103c70656d1130
Timur Churov <brutalnumberboy@omg.lol>
2025-04-17 07:07:48 +0200

    implement authentication and authorization

--- RecordStoreUI/src/app/homepage/homepage.component.ts
+++ RecordStoreUI/src/app/homepage/homepage.component.ts
@@ -1,24 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { AlbumService } from '../album.service';
+import { AuthorizationService } from '../authorization.service';
 import { RouterLink } from '@angular/router';
+import { UserDTO } from '../authorization/authorization.component';
+
 
 @Component({
   selector: 'app-homepage',
   imports: [CommonModule, RouterLink],
   standalone: true,
-  providers: [AlbumService],
+  providers: [AlbumService, AuthorizationService],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent implements OnInit {
   randomAlbums: any[] = [];
+  currentUser: UserDTO | null = null;
 
-  constructor(private albumService: AlbumService) {
-  }
+  constructor(private albumService: AlbumService, private authorizationService: AuthorizationService) { }
 
   ngOnInit(): void {
     this.getRandomAlbums();
+    const observer = {
+      next: (data: any) => {
+        console.log('Current user:', data);
+        this.currentUser = data;
+      },
+      error: (error: any) => {
+        console.error('Error fetching current user:', error);
+      }
+    }
+    this.authorizationService.getCurrentUser().subscribe(observer);
+    console.log('Current user:', this.currentUser);
   }
 
   getRandomAlbums(): void {
@@ -33,4 +47,16 @@ export class HomepageComponent implements OnInit {
     this.albumService.getRandomAlbums(4).subscribe(observer);
   }
 
+  logout(): void {
+    const observer = {
+      next: (data: any) => {
+        console.log('Logout successful', data);
+        this.currentUser = null;
+      },
+      error: (error: any) => {
+        console.error('Logout error', error);
+      }
+    }
+    this.authorizationService.logout().subscribe(observer);
+  }
 }
--- RecordStoreUI/src/app/search/search.component.ts
+++ RecordStoreUI/src/app/search/search.component.ts
@@ -3,23 +3,37 @@ import { CommonModule } from '@angular/common';
 import { AlbumService } from '../album.service';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { UserDTO } from '../registration/registration.component';
+import { AuthorizationService } from '../authorization.service';
 
 @Component({
   selector: 'app-search',
   imports: [CommonModule, FormsModule, RouterLink],
   standalone: true,
-  providers: [AlbumService],
+  providers: [AlbumService, AuthorizationService],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent implements OnInit{
   searchResults: any[] = [];
   searchQuery: string = '';
+  currentUser: UserDTO | null = null;
   
-  constructor(private albumService: AlbumService) {
+  constructor(private albumService: AlbumService, private authorizationService: AuthorizationService) {
   }
 
   ngOnInit(): void {
+    const observer = {
+      next: (data: any) => {
+        console.log('Current user:', data);
+        this.currentUser = data;
+      },
+      error: (error: any) => {
+        console.error('Error fetching current user:', error);
+      }
+    }
+    this.authorizationService.getCurrentUser().subscribe(observer);
+    console.log('Current user:', this.currentUser);
   }
   
 
@@ -38,6 +52,17 @@ export class SearchComponent implements OnInit{
         this.albumService.searchAlbumByTitleOrArtist(this.searchQuery.trim()).subscribe(observer);
     }
   
-
+  logout(): void {
+    const observer = {
+      next: (data: any) => {
+        console.log('Logout successful', data);
+        this.currentUser = null;
+      },
+      error: (error: any) => {
+        console.error('Logout error', error);
+      }
+    }
+    this.authorizationService.logout().subscribe(observer);
+  }
   
 }
