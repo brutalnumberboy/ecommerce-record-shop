4afd1fe31dd43fd2b18096d3664ad592813fa643
Timur Churov <brutalnumberboy@omg.lol>
2025-04-17 07:18:32 +0200

    adjust routing

--- RecordStoreUI/src/app/app.routes.ts
+++ RecordStoreUI/src/app/app.routes.ts
@@ -1,7 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SearchComponent } from './search/search.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AlbumPageComponent } from './album-page/album-page.component';
+import { BasketComponent } from './basket/basket.component';
 
 export const routes: Routes = [
     {path: '', component: HomepageComponent},
-    { path: '**', redirectTo: '' }, // Redirect unknown routes to the homepage
-];
+    {path: 'search', component: SearchComponent},
+    {path: 'albums', component: AlbumsComponent},
+    {path: 'albums/:id', component: AlbumPageComponent},
+    {path: 'login', component: AuthorizationComponent},
+    {path: 'register', component: RegistrationComponent},
+    {path: 'basket', component: BasketComponent},
+    { path: '**', redirectTo: '' }
+]
