22900dbfbbf3a4cb83256e803fc3db33940be4d7
Timur Churov <brutalnumberboy@omg.lol>
2025-04-17 07:18:08 +0200

    update parameter passing logic to filter function

--- RecordStoreUI/src/app/album.service.ts
+++ RecordStoreUI/src/app/album.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AlbumService {
-  private apiUrl = 'http://localhost:5230/api/album';
+  private apiUrl = 'https://localhost:7000/api/album';
 
   constructor(private http: HttpClient) {}
 
-  getRandomAlbums(count: number = 2): Observable<any> {
+  getAllAlbums(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/all`);
+  }
+  getRandomAlbums(count: number = 1): Observable<any> {
     return this.http.get(`${this.apiUrl}/random`, { params: { count: count.toString() } });
+  }
+  searchAlbumByTitleOrArtist(query: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/search?query=${query}`);
+  }
+  filterAlbumsByGenreOrYear(genre: string[], year: number[]): Observable<any> {
+    let params = new HttpParams();
+    genre.forEach((g) => {
+      params = params.append('genre', g);
+    });
+    year.forEach((y) => {
+      params = params.append('year', y.toString());
+    });
+    return this.http.get(`${this.apiUrl}/filter`, { params });
+  }
+  getAlbumById(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
   }
 }
